d3fa4b13bd858bb2e64d7feb03dc49ce02466db5
Ismail Ceylan <ismaillceylan@gmail.com>
2025-07-08 19:14:36 +0300

    fix: require app bootstrapper file

--- public/index.php
+++ public/index.php
@@ -13,4 +13,17 @@ define( 'GaravelStart', microtime( true ));
 |
 */
 
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
+
+/*
+|--------------------------------------------------------------------------
+| Run The Application
+|--------------------------------------------------------------------------
+|
+| Once we have the application, we can handle the incoming request using
+| the application's HTTP kernel. Then, we will send the response back
+| to this client's browser, allowing them to enjoy our application.
+|
+*/
+
+$app = require_once __DIR__ . '/../bootstrap/app.php';
